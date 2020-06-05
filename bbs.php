7f6b05b06f0b2d5611a11422fe2143bcbfa8937d
kenta1114 <61451524+kenta1114@users.noreply.github.com>
2020-06-05 15:29:52 +0900

    Add files via upload

---
+++ bbs.php
@@ -0,0 +1,97 @@
+<?php
+$dataFile='bbs.dat';
+
+function h($s){
+  return htmlspecialchars($s,ENT_QUOTES,'UTF-8');
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' &&
+    isset($_POST['message']) &&
+    isset($_POST['user'])){
+
+      $message=trim($_POST['message']);
+      $user=trim($_POST['user']);
+
+      if($message !==''){
+
+          $user=($user === '')? 'ななしさん':$user;
+
+          $message=str_replace("\t",'',$message);
+          $user=str_replace("\t",'',$user);
+
+          $postedAt=date('Y-m-d H:i:s');
+          date_default_timezone_get(Asia/Tokyo);
+
+
+          $newData = $message ."\t". $user ."\t". $postedAt."\n";
+
+          $fp=fopen($dataFile,'a');
+          fwrite($fp,$newData);
+          fclose($fp);
+
+
+      }
+}
+
+$posts=file($dataFile,FILE_IGNORE_NEW_LINES);
+
+$posts=array_reverse($posts);
+
+ ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>簡易掲示板</title>
+          <style>
+          body{
+            font-family: sans-serif;
+            border-top: 1px solid;
+            border-bottom: 1px solid;
+            border-left: 1px solid;
+            border-right: 1px solid;
+          }
+
+          h1{
+            text-align: center;
+            color: #00F;
+            line-height: 2.0;
+            padding-bottom: 10px
+          }
+          h2{
+            color: #00F;
+            font-size: 22px;
+            text-align: center;
+          }
+          form,ul{
+            list-style: none;
+            text-align: center;
+          }
+          </style>
+  </head>
+  <body>
+    <h1>簡易掲示板</h1>
+    <form action="" method="post">
+        message:<input type="text" name="message">
+        user:<input type="text" name="user">
+        <input type="submit" value="投稿">
+    </form>
+    <h2>投稿一覧(<?php echo count($posts); ?>件)</h2>
+    <ul>
+      <?php if(count($posts)) :?>
+        <?php foreach ($posts as $post) :?>
+        <?php list($message,$user,$postedAt)=explode("\t",$post);?>
+          <li>
+            <?php echo h($message); ?> <?php echo h($user); ?> <?php echo h($postedAt); ?>
+          </li>
+        <?php endforeach; ?>
+        <?php else :?>
+          <li>まだ投稿はありません。</li>
+      <?php endif; ?>
+    </ul>
+  </body>
+</html>
